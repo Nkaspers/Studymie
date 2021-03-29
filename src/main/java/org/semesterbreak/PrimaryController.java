7d9dc24949ea159ba837e3b9133be08f1ada85a2
tenzens <tenzens.github@gmail.com>
2021-03-29 20:54:38 +0200

    Started working on Edit Mode and added Icons

--- src/main/java/org/semesterbreak/PrimaryController.java
+++ src/main/java/org/semesterbreak/PrimaryController.java
@@ -1,7 +1,12 @@
 package org.semesterbreak;
 
 import java.io.IOException;
+import java.util.Properties;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
 public class PrimaryController {
 
