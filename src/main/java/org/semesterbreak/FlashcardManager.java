062d1ed3ce1ea02b3a91e1ed28140fc74610ea92
tenzens <tenzens.github@gmail.com>
2021-04-02 21:29:16 +0200

    Added FlashcardManager

---
+++ src/main/java/org/semesterbreak/FlashcardManager.java
@@ -0,0 +1,20 @@
+package org.semesterbreak;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FlashcardManager {
+    public List<FlashcardStack> stackList = new ArrayList<>(Arrays.asList(new FlashcardStack("Stack 1"), new FlashcardStack("Stack 2")));
+
+    public List<FlashcardStack> getStackList(){
+        return stackList;
+    }
+
+    public Flashcard addFlashcard(FlashcardStack stack) {
+        Flashcard flashcard = new Flashcard("untitled");
+        stack.getFlashcards().add(flashcard);
+        return flashcard;
+    }
+
+}
