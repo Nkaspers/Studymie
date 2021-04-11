673c081770e6b014e6dbdf1c0ebccb74609ac59f
tenzens <tenzens.github@gmail.com>
2021-04-11 16:31:19 +0200

    Finalized WelcomeScene

--- src/main/java/org/semesterbreak/scenes/welcome/WelcomeController.java
+++ src/main/java/org/semesterbreak/scenes/welcome/WelcomeController.java
@@ -1,7 +1,10 @@
 package org.semesterbreak.scenes.welcome;
 
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import org.jdom2.JDOMException;
 import org.semesterbreak.Utilities;
 
@@ -12,12 +15,27 @@ public class WelcomeController {
     public Button addStackButton;
     public Button openProjectButton;
 
-    public void initialize() throws JDOMException, IOException {
-        Group stackIcon = Utilities.getIconGroup("add_stack_white.svg", true);
-        Group openIcon = Utilities.getIconGroup("open.svg", true);
-        stackIcon.setScaleX(1.5);
-        stackIcon.setScaleY(1.5);
-        addStackButton.setGraphic(stackIcon);
-        openProjectButton.setGraphic(openIcon);
+    public void initialize() {
+        initializeIcons();
+    }
+
+    private void initializeIcons() {
+        try {
+            Group stackIcon = Utilities.getIconGroup("add_stack_white.svg", true);
+            var stackAnchorpane = ((AnchorPane) addStackButton.getGraphic());
+            Label stackGraphic = (Label) stackAnchorpane.getChildren().get(0);
+            stackIcon.setScaleX(1.5);
+            stackIcon.setScaleY(1.5);
+            stackGraphic.setGraphic(stackIcon);
+
+            Group openIcon = Utilities.getIconGroup("open.svg", true);
+            var openAnchorpane = ((AnchorPane) openProjectButton.getGraphic());
+            Label openGraphic = (Label) openAnchorpane.getChildren().get(0);
+            openGraphic.setScaleX(1.5);
+            openGraphic.setScaleY(1.5);
+            openGraphic.setGraphic(openIcon);
+        }catch (JDOMException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
